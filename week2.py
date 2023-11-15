230e49134bb3cb44e6aeb4d077037898cc21b2f8
Dennis Mejia <mejiade@vcu.edu>
2023-11-14 19:27:46 -0500

    challenge 2

---
+++ week2.py
@@ -0,0 +1,20 @@
+
+with open('message_02.txt',"r") as myfile:
+    data = list(myfile.read())
+
+value=0
+
+value = 0
+
+for symbol in data:
+    
+    if symbol == '#':
+        value += 1
+    if symbol == '@':
+        value -= 1
+    if symbol == '*':
+        value *= value
+    if symbol == '&':
+        print(value, end='')
+
+
